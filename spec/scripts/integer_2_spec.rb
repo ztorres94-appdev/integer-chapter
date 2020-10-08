f27fd86491322fe84568683e45bdceb00617e4ca
Jelani Woods <jelani@firstdraft.com>
2020-10-08 15:30:07 -0500

    Update spec titles

--- spec/scripts/integer_2_spec.rb
+++ spec/scripts/integer_2_spec.rb
@@ -11,7 +11,7 @@ describe "integer_odd.rb" do
 end
 
 describe "integer_birth_year.rb" do
-  it "should output'Wow, you were born in 1940. You're old!' if the input is 80.", points: 1 do
+  it "should output 'Wow, you were born in 1940. You're old!' if the input is 80.", points: 1 do
     
     allow_any_instance_of(Object).to receive(:gets).and_return("80")
 
@@ -20,7 +20,7 @@ describe "integer_birth_year.rb" do
 end
 
 describe "integer_birth_year.rb" do
-  it "should output'Wow, you were born in 1940. You're old!' if the input is 80.", points: 1 do
+  it "should output 'Wow, you were born in 2000. You're old!' if the input is 20.", points: 1 do
     # Un-require integer_birth_year.rb
     integer_birth_year = $".select{|r| r.include? 'integer_birth_year.rb'}
     $".delete(integer_birth_year.first)
