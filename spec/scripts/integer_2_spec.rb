05bf0cb7cdb0a046c8277225534272e1fc61a476
Jelani Woods <jelani@firstdraft.com>
2021-04-08 16:30:44 -0500

    Update birth year tests

--- spec/scripts/integer_2_spec.rb
+++ spec/scripts/integer_2_spec.rb
@@ -1,3 +1,5 @@
+require "date"
+
 describe "integer_odd.rb" do
   it "should output 'false' if the entered number is not odd", points: 1 do
     # Un-require integer_odd.rb
@@ -12,10 +14,10 @@ end
 
 describe "integer_birth_year.rb" do
   it "should output 'Wow, you were born in 1940. You're old!' if the input is 80.", points: 1 do
-    
-    allow_any_instance_of(Object).to receive(:gets).and_return("80")
 
-    expect { require_relative '../../integer_birth_year' }.to output(/Wow, you were born in 1940. You're old!/i).to_stdout
+    allow_any_instance_of(Object).to receive(:gets).and_return("80")
+    year = Date.today.year - 80
+    expect { require_relative '../../integer_birth_year' }.to output(/Wow, you were born in #{year}. You're old!/i).to_stdout
   end
 end
 
@@ -26,7 +28,7 @@ describe "integer_birth_year.rb" do
     $".delete(integer_birth_year.first)
 
     allow_any_instance_of(Object).to receive(:gets).and_return("20")
-
-    expect { require_relative '../../integer_birth_year' }.to output(/Wow, you were born in 2000. You're old!/i).to_stdout
+    year = Date.today.year - 20
+    expect { require_relative '../../integer_birth_year' }.to output(/Wow, you were born in #{year}. You're old!/i).to_stdout
   end
 end
